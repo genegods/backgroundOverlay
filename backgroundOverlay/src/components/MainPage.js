96a1935b5c716010c368bde788798ee30892d304
Good Do <56465252+genegods@users.noreply.github.com>
2022-12-04 09:41:47 +0800

    Add files via upload

---
+++ backgroundOverlay/src/components/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+   
+  <React.Fragment>
+  <section>
+    <div className='wrapper centered text-white text-5xl md:text-8xl'>
+    <p>Background Overlay</p>
+    </div>
+  </section>
+</React.Fragment>
+  )
+}
+
+export default MainPage
